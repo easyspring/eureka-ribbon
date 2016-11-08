dd60e06c81aadb5a49f09265757818286ac1ab10
胡家宝 <jiabao_hu@corp.netease.com>
2016-11-08 16:29:20 +0800

    first

---
+++ src/main/java/com/netease/RibbonApplication.java
@@ -0,0 +1,24 @@
+package com.netease;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RibbonApplication 
+{
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+    public static void main( String[] args )
+    {
+        new SpringApplicationBuilder(RibbonApplication.class).web(true).run(args);
+    }
+}
