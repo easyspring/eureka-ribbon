8785875cebb6453d9cbb6ad19712dead7e565e30
胡家宝 <jiabao_hu@corp.netease.com>
2016-11-08 17:12:43 +0800

    hystrix, callback

--- src/main/java/com/netease/web/ConsumerController.java
+++ src/main/java/com/netease/web/ConsumerController.java
@@ -4,16 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import com.netease.services.ComputeService;
 
 @RestController
 public class ConsumerController {
 
 	@Autowired
-	private RestTemplate restTemplate;
+	private ComputeService computeService;
 	
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
 	public String add(){
-		return restTemplate.getForEntity("http://COMPUTE-SERVICE-ME/add?a=10&b=20", String.class).getBody();
+		return computeService.addService();
 	}
 }
